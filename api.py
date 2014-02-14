dc82e4e359fe0bede98de50b3a651a49e5e92a00
Javier de la Rosa <versae@gmail.com>
2014-02-14 17:40:27 -0500

    Add templates and javascript code to capture an image form the webcam

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from flask.ext import restful
 
 app = Flask(__name__)
@@ -9,13 +10,22 @@ class FaceSimilarity(restful.Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(FaceSimilarity, '/')
+api.add_resource(FaceSimilarity, '/api/')
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello World!'
+@app.route('/')
+def home():
+    return render_template('home.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
