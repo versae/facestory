ef653833607d216e6ccb8e0ea33c5aef7527ef41
Javier de la Rosa <versae@gmail.com>
2014-02-17 23:44:40 -0500

    Adding SkyBiometry API requests

--- api.py
+++ api.py
@@ -1,11 +1,76 @@
+# -*- coding: utf-8 -*-
+import cStringIO
+import os
+import re
+import uuid
+
+import requests
+
+from flask import request
 from flask.ext import restful
+from flask.ext.restful import reqparse
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+SKYBIOMETRY_API_KEY = os.environ['SKYBIOMETRY_API_KEY']
+SKYBIOMETRY_API_SECRET = os.environ['SKYBIOMETRY_API_SECRET']
 
 
 class FaceSimilarity(restful.Resource):
+
     def get(self):
         return {'hello': 'world'}
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('data_uri', type=str, required=True,
+                            help='Data URI with the image of a face')
+        args = parser.parse_args()
+        # Get image from request
+        data_uri = args['data_uri']
+        image_id = uuid.uuid4().hex
+        image_name = u"{}.png".format(image_id)
+        image_path = os.path.join(PROJECT_ROOT, "static", "faces", image_name)
+        image_url = u"/static/faces/{}".format(image_name)
+        image_str = re.search(r'base64,(.*)', data_uri).group(1)
+        image_base64 = image_str.decode('base64')
+        # Save image in disk
+        message = u"OK"
+        try:
+            output = open(image_path, 'wb')
+            output.write(image_base64)
+            output.close()
+        except IOError:
+            message = u"Error when saving image in disk"
+        # Create image in memory
+        image_tmp = cStringIO.StringIO(image_base64)
+        image_faces = get_image_faces(image_tmp)
+        #im = Image.open(image_tmp)
+        return {
+            'image_url': image_url,
+            'host': request.host,
+            'message': message,
+            'faces': image_faces,
+        }
 
 
 def add_api(app):
     api = restful.Api(app)
-    api.add_resource(FaceSimilarity, '/api/')
+    api.add_resource(FaceSimilarity, '/api/similarity')
+
+
+def get_image_faces(file_obj):
+    url = u"http://api.skybiometry.com/fc/faces/detect.json"
+    data = {
+        "api_key": SKYBIOMETRY_API_KEY,
+        "api_secret": SKYBIOMETRY_API_SECRET,
+        "urls": "",
+        "attributes": "all",
+    }
+    files = {'file': file_obj}
+    response = requests.post(url, data=data, files=files)
+    json = {}
+    if response.status_code == requests.codes.ok:
+        json = response.json()
+        if json["status"] == u"success":
+            return json["photos"][0]
+    return None
---
+++ static/js/main.js
@@ -0,0 +1,155 @@
+/*jshint
+    forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true,
+    undef:true, curly:true, browser:true, indent:4, maxerr:50, strict:false
+*/
+
+/*global
+    canvas, alert, console, video
+*/
+
+'use strict';
+
+// Take a screenshot using getUserMedia API.
+// From http://www.miguelmota.com/blog/screenshots-with-getusermedia-api/
+// Give credit where credit is due. The code is heavily inspired by
+// HTML5 Rocks' article "Capturing Audio & Video in HTML5"
+// http://www.html5rocks.com/en/tutorials/getusermedia/intro/
+(function ($, document, undefined) {
+
+    // Our element ids.
+    var options = {
+        video: '#video',
+        canvas: '#canvas',
+        captureBtn: '#capture-btn',
+        imageURLInput: '#image-url-input'
+    };
+    // This will hold the video stream.
+    var localMediaStream = null;
+    // This will hold the screenshot base 64 data url.
+    var dataURL = null;
+    // This will hold the converted PNG url.
+    var imageURL = null;
+    // Our object that will hold all of the functions.
+    var App = {
+        // Get the video element.
+        video: document.querySelector(options.video),
+        // Get the canvas element.
+        canvas: document.querySelector(options.canvas),
+        // Get the canvas context.
+        ctx: canvas.getContext('2d'),
+        // Get the capture button.
+        captureBtn: document.querySelector(options.captureBtn),
+        // Get the input field to paste in the imageURL.
+        imageURLInput: document.querySelector(options.imageURLInput),
+
+        initialize: function () {
+            var that = this;
+            // Check if navigator object contains getUserMedia object.
+            navigator.getUserMedia = (
+                navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia || navigator.msGetUserMedia
+            );
+            // Check if window contains URL object.
+            window.URL = window.URL || window.webkitURL;
+
+            // Check for getUserMedia support.
+            if (navigator.getUserMedia) {
+                // Get video stream.
+                navigator.getUserMedia({
+                    video: true
+                }, this.gotStream, this.noStream);
+
+                // Bind capture button to capture method.
+                this.captureBtn.onclick = function () {
+                    that.capture();
+                };
+            } else {
+                // No getUserMedia support.
+                alert('Your browser does not support getUserMedia API.');
+            }
+        },
+
+        // Stream error.
+        noStream: function (err) {
+            alert('Could not get camera stream.');
+            console.log('Error: ', err);
+        },
+
+        // Stream success.
+        gotStream: function (stream) {
+            // Feed webcam stream to video element.
+            // IMPORTANT: video element needs autoplay attribute or it will be
+            // frozen at first frame.
+            if (window.URL) {
+                video.src = window.URL.createObjectURL(stream);
+            } else {
+                video.src = stream; // Opera support.
+            }
+
+            // Store the stream.
+            localMediaStream = stream;
+        },
+
+        // Capture frame from live video stream.
+        capture: function () {
+            var that = this;
+            // Check if has stream.
+            if (localMediaStream) {
+                // Draw whatever is in the video element on to the canvas.
+                that.ctx.drawImage(video, 0, 0);
+                // Create a data url from the canvas image.
+                dataURL = canvas.toDataURL('image/png');
+                // Call our method to save the data url to an image.
+                $(options.canvas).toggle();
+                $(options.video).toggle();
+                that.saveDataUrlToImage();
+            }
+        },
+
+        saveDataUrlToImage: function () {
+            var that = this;
+            var options = {
+                // Change this to your own url.
+                url: '/api/similarity'
+            };
+
+            // Only place where we need jQuery to make an ajax request
+            // to our server to convert the dataURL to a PNG image,
+            // and return the url of the converted image.
+            that.imageURLInput.value = 'Fetching url ...';
+            $.ajax({
+                url: options.url,
+                type: 'POST',
+                dataType: 'json',
+                data: { 'data_uri': dataURL },
+                complete: function (xhr, textStatus) {
+                // Request complete.
+                },
+                // Request was successful.
+                success: function (response, textStatus, xhr) {
+                    console.log('Response: ', response);
+                    // Conversion successful.
+                    if (response.status_code === 200) {
+                        imageURL = response.data.image_url;
+                        // Paste the PNG image url into the input field.
+                        that.imageURLInput.value = imageURL;
+                                                that.imageURLInput.removeAttribute('disabled');
+                    }
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    // Some error occured.
+                    console.log('Error: ', errorThrown);
+                    imageURLInput.value = 'An error occured.';
+                }
+            });
+        }
+
+    };
+
+    // Initialize our application.
+    App.initialize();
+
+    // Expose to window object for testing purposes.
+    window.App = App || {};
+
+})(jQuery, document);
